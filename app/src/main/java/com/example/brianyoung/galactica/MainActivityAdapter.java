b6168fe661cb23245b2e24dd06218eba2a26b8e1
Rafella <rafella_angeline@yahoo.com>
2020-03-27 22:18:08 +1100

    Adding planet pictures

--- app/src/main/java/com/example/brianyoung/galactica/MainActivityAdapter.java
+++ app/src/main/java/com/example/brianyoung/galactica/MainActivityAdapter.java
@@ -4,6 +4,7 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -24,6 +25,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
 
     public static class MainActivityViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         public TextView planetName;
+        public ImageView planetPic;
         private RecyclerViewClickListener mListener;
 
         public MainActivityViewHolder(View v, RecyclerViewClickListener listener) {
@@ -32,6 +34,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
             mListener = listener;
             v.setOnClickListener(this);
             planetName = v.findViewById(R.id.planetNameList);
+            planetPic = v.findViewById(R.id.planetPicList);
 
         }
 
@@ -51,6 +54,7 @@ public class MainActivityAdapter extends RecyclerView.Adapter<MainActivityAdapte
     public void onBindViewHolder(MainActivityViewHolder holder, int position) {
         Planet planet = mPlanets.get(position);
         holder.planetName.setText(planet.getName());
+        //holder.planetPic.setImageResource();
     }
 
     // Return the size of your dataset (invoked by the layout manager)
--- app/src/main/java/com/example/brianyoung/galactica/Planet.java
+++ app/src/main/java/com/example/brianyoung/galactica/Planet.java
@@ -20,14 +20,14 @@ public class Planet {
 
     public static ArrayList<Planet> getPlanets(){
         ArrayList<Planet> planets = new ArrayList<>();
+        planets.add(new Planet("Mercury"));
+        planets.add(new Planet("Venus"));
         planets.add(new Planet("Earth"));
+        planets.add(new Planet("Mars"));
         planets.add(new Planet("Jupiter"));
         planets.add(new Planet("Saturn"));
-        planets.add(new Planet("Mars"));
-        planets.add(new Planet("Neptune"));
         planets.add(new Planet("Uranus"));
-        planets.add(new Planet("Venus"));
-        planets.add(new Planet("Mercury"));
+        planets.add(new Planet("Neptune"));
         planets.add(new Planet("Pluto"));
         return planets;
     }
