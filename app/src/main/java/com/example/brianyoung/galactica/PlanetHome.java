28bb2e8e08b01ab42d82e34312aa7b479b156b72
hillarymulyadi <mulyadihillary@gmail.com>
2020-03-28 18:04:21 +1100

    Completed Fragment onCreate and onCreateView methods

--- app/src/main/java/com/example/brianyoung/galactica/PlanetHome.java
+++ app/src/main/java/com/example/brianyoung/galactica/PlanetHome.java
@@ -1,62 +1,41 @@
 package com.example.brianyoung.galactica;
 
-import android.content.Context;
-import android.net.Uri;
+
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link PlanetHome.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link PlanetHome#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class PlanetHome extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+
+    private static final String ARG_PLANET_NAME = "param1";
 
     // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private Planet planet;
+    private Button btnShowVideo, btnMoreInfo;
+    private ImageView clickableImage;
+    private TextView planetName;
 
-    private OnFragmentInteractionListener mListener;
 
     public PlanetHome() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment PlanetHome.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static PlanetHome newInstance(String param1, String param2) {
-        PlanetHome fragment = new PlanetHome();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+
+        //get arguments from activity
+        if(getArguments().containsKey(ARG_PLANET_NAME)){ //receiving the bundle arguments from the activity this fragment is associated with
+            planet = Planet.getPlanet(getArguments().getString(ARG_PLANET_NAME));
+            this.getActivity().setTitle(planet.getName());
         }
     }
 
@@ -64,45 +43,36 @@ public class PlanetHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_planet_home, container, false);
+        View v = inflater.inflate(R.layout.fragment_planet_home, container, false);
+
+        //TODO: Initialise the fragment's UI elements by finding them in the fragment layout
+        btnShowVideo = v.findViewById(R.id.btnShowVideo);
+        planetName = v.findViewById(R.id.planetName);
+        btnMoreInfo = v.findViewById(R.id.btnMoreInfo);
+        clickableImage = v.findViewById(R.id.clickableImage);
+
+        //TODO: Display the details of the selected City using the fragment's UI elements
+        btnShowVideo.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                //Intent code to go to activity 3
+
+
+
+            }
+        });
+
+
+
+        return v;
     }
 
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
+    //method to go to activity 3 (show video)
+//    public void launchShowVideo(Planet planet, int position){
+//        Intent intent = new Intent(this, //activity3.class);
+//        intent.putExtra()
+//
+//    }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
-
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
