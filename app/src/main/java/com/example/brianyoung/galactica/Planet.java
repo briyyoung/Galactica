428ca9f0f979110459bb1e61f8c414ad6bc10b30
Rafella <rafella_angeline@yahoo.com>
2020-03-28 18:14:37 +1100

    add pictures in drawable

--- app/src/main/java/com/example/brianyoung/galactica/Planet.java
+++ app/src/main/java/com/example/brianyoung/galactica/Planet.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 public class Planet {
 
     private String name;
+    private int picture;
 
     public String getName() {
         return name;
@@ -14,8 +15,18 @@ public class Planet {
         this.name = name;
     }
 
-    public Planet(String name){
+    public int getPicture() {
+        return picture;
+    }
+
+    public void setPicture(int picture) {
+        this.picture = picture;
+    }
+
+    //constructor for planet
+    public Planet(String name, int picture){
         this.name = name;
+        this.picture = picture;
     }
 
     //create method that would go get you one planet.
@@ -28,18 +39,17 @@ public class Planet {
         return null;
     }
 
-
     public static ArrayList<Planet> getPlanets(){
         ArrayList<Planet> planets = new ArrayList<>();
-        planets.add(new Planet("Mercury"));
-        planets.add(new Planet("Venus"));
-        planets.add(new Planet("Earth"));
-        planets.add(new Planet("Mars"));
-        planets.add(new Planet("Jupiter"));
-        planets.add(new Planet("Saturn"));
-        planets.add(new Planet("Uranus"));
-        planets.add(new Planet("Neptune"));
-        planets.add(new Planet("Pluto"));
+        planets.add(new Planet("Mercury",1));
+        planets.add(new Planet("Venus",2));
+        planets.add(new Planet("Earth",3));
+        planets.add(new Planet("Mars",4));
+        planets.add(new Planet("Jupiter",5));
+        planets.add(new Planet("Saturn",6));
+        planets.add(new Planet("Uranus",7));
+        planets.add(new Planet("Neptune",8));
+        planets.add(new Planet("Pluto",9));
         return planets;
     }
 
