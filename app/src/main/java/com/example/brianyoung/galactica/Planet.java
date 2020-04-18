779823aa1559f001fd3e610a38ed0a7d16652301
hillarymulyadi <mulyadihillary@gmail.com>
2020-04-19 01:00:35 +1000

    Fixed planet desc grammar + added Pluto desc

--- app/src/main/java/com/example/brianyoung/galactica/Planet.java
+++ app/src/main/java/com/example/brianyoung/galactica/Planet.java
@@ -53,23 +53,23 @@ public class Planet {
     public static ArrayList<Planet> getPlanets(){
         ArrayList<Planet> planets = new ArrayList<>();
         planets.add(new Planet("Mercury",1,
-                "Mercury is the closest planet to the sun with 88 earth days of orbiting the sun. Having extreme changes in temperature during the day which could be as high up to 450c and very low at night as low as minus 180c. The diameter of Mercury is 4878km."));
+                "Mercury is the closest planet to the Sun with 88 Earth days of orbiting the sun. It has extreme changes in temperature during the day which could be as high as 450 degree celsius and as low as minus 180 degree celsius at night. The diameter of Mercury is 4878km."));
         planets.add(new Planet("Venus",2,
-                "Venus is the second planet from the sun with an orbit time of 225 earth days. The average temperature is relatively very high which is 465c and the diameter of Venus is 12,104km. The spin direction is opposite to other planets which are from east to west."));
+                "Venus is the second closest planet to the sun with an orbit time of 225 earth days. The average temperature is relatively high ranging to 465 degree celsius. It has a diameter of 12,104km. The spin direction goes in the opposite direction to other planets which usually go from east to west."));
         planets.add(new Planet("Earth",3,
-                "Earth is the only habitable planet among the solar system. Yes, it is the planet where we live now. The diameter of the earth is 12,760km with an orbit time of 365.24 days which make up one calendar year of our time."));
+                "The planet we live in, Earth, is the only habitable planet among the solar system. Earth has a diameter of 12,760km with an orbit time of 365.24 days which make up one calendar year of our time."));
         planets.add(new Planet("Mars",4,
-                "The fourth planet from the sun with orbit time of 687 earth days, that has a diameter of 6,787km. Mars is covered with iron oxides dust which makes the planet have a red hue. The nickname of Mars is “Red Planet”."));
+                "Mars is the fourth planet from the sun with an orbit time of 687 earth days, that has a diameter of 6,787km. Mars is covered with iron oxide dust which make the planet gleam with a red hue. Often referred to as the “Red Planet”."));
         planets.add(new Planet("Jupiter",5,
-                "The fifth planet from the sun with orbit time of 11.9 earth days and the Largest planet in our solar system with a diameter of 139,822km. Jupiter has 75 moons orbiting the planet and has swirling clouds which called Great Red Spot."));
+                "The fifth planet from the sun with orbit time of 11.9 earth days and the Largest planet in our solar system with a diameter of 139,822km. Jupiter has 75 moons orbiting the planet and has swirling clouds called the Great Red Spot."));
         planets.add(new Planet("Saturn",6,
-                "The sixth planet from the sun that has visible ring that made from ice and rock with orbit time of 29.5 earth years. The diameter of the planet is 120,500km."));
+                "The sixth planet from the sun with a visible ring made of ice and rock with an orbit time of 29.5 earth years. The diameter of the planet is 120,500km."));
         planets.add(new Planet("Uranus",7,
-                "The seventh planet from the sun that has the orbit time of 84 years and diameter  51,120km. The colour of the planet seen from earth is blue-greenish due to the atmosphere built from methane."));
+                "The seventh planet from the sun that has the orbit time of 84 years and a diameter of 51,120km. The colour of the planet observed from earth is blue-greenish due to the atmosphere built from methane."));
         planets.add(new Planet("Neptune",8,
-                "The eighth planet from the sun that has an orbit time of 165 earth years with a diameter of 49,530 km that similar to Uranus. Neptune has very supersonic strong wind."));
+                "The eighth planet from the sun that has an orbit time of 165 earth years with a diameter of 49,530 km, similar to that of Uranus. Neptune is known for having supersonic strong winds - the strongest winds in the solar system."));
         planets.add(new Planet("Pluto",9,
-                "Pluto description"));
+                "Smaller than Earth's moon, Pluto is known as the \"Drarf planet\". This planet contains extremely high mountains, and even has red snow."));
         return planets;
     }
 
